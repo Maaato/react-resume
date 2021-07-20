e459e13c5291581b54830b470d83915df9d2066a
Maaato <lagos.matias25@gmail.com>
2021-07-20 13:48:19 -0400

    [Refactor] Data.js Updated

--- src/Schemas/Data.js
+++ src/Schemas/Data.js
@@ -3,7 +3,7 @@ export const Data = {
     name: "Matias Lagos",
     ocupation: "FullStack Software Developer",
     location: "Santiago, Chile",
-    email: "mlagos.dev@protonmail.com",
+    email: "lagos.matias25@gmail.com",
     telephone: "+56 971999705",
     image: "images/90208122_10222269974249078_3631065450546200576_n.jpg",
   },
@@ -68,9 +68,10 @@ export const Data = {
         period: "Oct. 2019 - Actualidad",
         company: "Altiuz",
         description: [
-          "Desarrollo de Web Services REST en Java y Nodejs en Javascript como Typescript acompañado de diferentes motores de BD como Mysql, Oracle, MariaDB, MongoDB y IBM API Connect como API Gateway.",
-          "Desarrollo de aplicaciones tanto Web como Mobile con tecnologias como Angular, React, React Native, Ionic, entre otras tecnologías.",
-          "Uso de herramientas PaaS complementado con CI/CD como Openshift, Heroku, Vercel.",
+          "Desarrollo de Web Services REST en Java y Nodejs(JS/TS) acompañado de diferentes motores de BD como Mysql, Oracle, MariaDB, MongoDB e IBM API Connect y 3scale como API Gateway.",
+          "Desarrollo de Frontend y Mobile con tecnologías como Angular, React, React Native, Ionic y Cordova.",
+          "Despliegues PaaS: Openshift, Heroku, Railway, Vercel complementado con CI/CD.",
+          "Despliegues AS: Jboss/Wildfly, IBM Websphere, Oracle Weblogic, Tomcat, Glassfish.",
         ],
       },
     ],
@@ -78,24 +79,25 @@ export const Data = {
       {
         career: "Técnico de Nivel Superior Analista Programador",
         date: "2019",
-        institution: "Universidad Tecnológica de Chile",
+        institution: "Universidad Tecnológica de Chile INACAP",
       },
     ],
     proyects: [
       {
-        name: "Outsourcing",
+        name: "Onboarding Digital",
         company: "Banco de Chile",
         period: "Nov. 2019 - Ene. 2020",
         description: [
-          "Desarrollo, securitizacion y despliegue de APIs en plataforma IBM API Connect para la creación de un nuevo producto del banco.",
+          "Control, desarrollo, securitización y despliegue de APIs en plataforma IBM API Connect.",
         ],
       },
       {
-        name: "Outsourcing",
+        name: "Captación Digital",
         company: "Banco Ripley",
         period: "Feb. 2020",
         description: [
-          "Desarrollo, securitizacion y documentación de APIs en plataforma IBM API Connect para la creación de un nuevo producto del banco.",
+          "Control, desarrollo y securitización de APIs en plataforma IBM API Connect.",
+          "Desarrollo frontend en Angular para el consumo de Web Services SOAP/REST apuntando a ambientes DEV/PRE-PROD/PROD como simulación.",
         ],
       },
       {
@@ -103,25 +105,25 @@ export const Data = {
         company: "Gobierno de Chile",
         period: "Mar. 2020 - May. 2020",
         description: [
-          "Desarrollo de aplicación web y mobile con las tecnologia de Angular 6 e Ionic 4 para el agendamiento de horas en centros a lo largo de todo Chile.",
+          "Desarrollo frontend y mobile con las tecnologias de Angular 6 e Ionic 4 para el agendamiento de horas en centros a lo largo de todo Chile.",
         ],
       },
       {
-        name: "Altiuz Report On Air",
+        name: "Altiuz Report Dynamic",
         company: "Altiuz",
         period: "Jun. 2020 - Jul. 2020",
         description: [
-          "Desarrollo servicios en Java para la generación unitaria de documentos, sin la necesidad de tener almacenada la información del mismo en una fuente de datos.",
+          "Desarrollo de servicio Java para la generación unitaria de documentos, sin la necesidad de tener almacenada la información del mismo en una fuente de datos.",
         ],
       },
       {
-        name: "Desarrollo Tigo",
+        name: "Desarrollos Tigo",
         company: "Computec S.A.S",
         period: "Ago. 2020 - Dic 2020",
         description: [
           "Desarrollo Java/Spring Boot de Web Service API REST para consulta y descarga de documentos almacenados en repositorios IBM CMOD.",
-          "Desarrollo Java CRON de transferencia de documentos XML hacia servidor SFTP.",
-          "Desarrollo Java CRON de carga de documentos PDFs hacia Azure Blob Storage.",
+          "Desarrollo Java CRON para transferencia de documentos XML hacia servidor SFTP.",
+          "Desarrollo Java CRON para carga de documentos PDFs hacia Azure Blob Storage.",
         ],
       },
     ],
